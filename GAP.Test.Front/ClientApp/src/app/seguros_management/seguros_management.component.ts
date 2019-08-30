ff2696dce3774b60f3a7ea132e490499526b52b7
mgutierrez <mgutierrez@YUXMED0010L>
2019-08-30 16:15:17 -0500

    add seguros management module

---
+++ GAP.Test.Front/ClientApp/src/app/seguros_management/seguros_management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seguros_management',
+  templateUrl: './seguros_management.component.html',
+  styleUrls: ['./seguros_management.component.css']
+})
+export class Seguros_managementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ GAP.Test.Front/ClientApp/src/app/seguros_management/seguros_management.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Seguros_managementComponent } from './seguros_management.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Seguros_managementComponent]
+})
+export class Seguros_managementModule { }
